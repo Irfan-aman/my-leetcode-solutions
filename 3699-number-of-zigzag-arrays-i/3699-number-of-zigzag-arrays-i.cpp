069f2632ce7194b76d1d50cd95188731eae2df91
Md Irfan Alam <alamirfan0781@gmail.com>
2026-06-24 13:23:54 +0530

    Time: 1440 ms (22.52%), Space: 717.2 MB (5.41%) - LeetHub

--- 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
+++ 3699-number-of-zigzag-arrays-i/3699-number-of-zigzag-arrays-i.cpp
@@ -1,46 +1,31 @@
 class Solution {
 public:
-    static constexpr int MOD = 1000000007;
-
+    typedef long long ll;
+    int MOD=1e9+7;
     int zigZagArrays(int n, int l, int r) {
-        int m = r - l + 1;
-
-        vector<int> inc(m), dec(m);
-
-        for (int i = 0; i < m; i++) {
-            inc[i] = i;
-            dec[i] = m - 1 - i;
+        int m=r-l+1;
+        ll dp[2001][2001][2];//DP Array
+        for(int prevVal=1;prevVal<=m;prevVal++){//Base Case
+            dp[n][prevVal][1]=1;
+            dp[n][prevVal][0]=1;
         }
-
-        for (int len = 3; len <= n; len++) {
-
-            vector<int> newInc(m), newDec(m);
-
-            long long pref = 0;
-            for (int i = 0; i < m; i++) {
-                newInc[i] = pref;
-                pref += dec[i];
-                if (pref >= MOD) pref %= MOD;
+        for(int i=n-1;i>=0;i--){
+            vector<ll> cumSum0(m+1,0);
+            vector<ll> cumSum1(m+1,0);
+            for(int val=1;val<=m;val++){
+                cumSum0[val]=(cumSum0[val-1]+dp[i+1][val][0])% MOD;
+                cumSum1[val]=(cumSum1[val-1]+dp[i+1][val][1])% MOD;
             }
-
-            long long suff = 0;
-            for (int i = m - 1; i >= 0; i--) {
-                newDec[i] = suff;
-                suff += inc[i];
-                if (suff >= MOD) suff %= MOD;
+            for(int prevVal=1;prevVal<=m;prevVal++){
+                dp[i][prevVal][1]=(cumSum0[m]-cumSum0[prevVal]+MOD)% MOD;
+                dp[i][prevVal][0]=cumSum1[prevVal-1];
             }
-
-            inc.swap(newInc);
-            dec.swap(newDec);
         }
-
-        long long ans = 0;
-        for (int i = 0; i < m; i++) {
-            ans += inc[i];
-            ans += dec[i];
-            ans %= MOD;
+        ll result=0;
+        for(int startVal=1;startVal<=m;startVal++){
+            result=(result+dp[1][startVal][1]) % MOD;
+            result =(result + dp[1][startVal][0]) % MOD;
         }
-
-        return ans;
+        return result;
     }
 };
