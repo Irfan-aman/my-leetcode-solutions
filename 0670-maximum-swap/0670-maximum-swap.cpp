163af1d3dc66e03730a1925388d70e771a8877b0
Md Irfan Alam <alamirfan0781@gmail.com>
2026-06-25 14:13:36 +0530

    Time: 0 ms (100%), Space: 8 MB (28.83%) - LeetHub

---
+++ 0670-maximum-swap/0670-maximum-swap.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s=to_string(num);
+        for(int i=0;i<s.size();i++){
+            int max=i+1;
+            for(int j=i+2;j<s.size();j++){
+                if(s[j]>=s[max]){
+                    max=j;
+                }
+            }
+            if(s[i]<s[max]){
+                swap(s[i],s[max]);
+                break;
+            }
+        }
+        return stoi(s);
+    }
+};
