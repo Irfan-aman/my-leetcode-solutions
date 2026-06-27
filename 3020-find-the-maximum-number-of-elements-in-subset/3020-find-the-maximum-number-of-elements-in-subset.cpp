25d7852c017e79b0908040c3521c853135103d70
Md Irfan Alam <alamirfan0781@gmail.com>
2026-06-27 11:32:47 +0530

    Time: 111 ms (49.14%), Space: 120.4 MB (61.71%) - LeetHub

---
+++ 3020-find-the-maximum-number-of-elements-in-subset/3020-find-the-maximum-number-of-elements-in-subset.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        map<int,int> mp;
+        for(int &x:nums) mp[x]++;
+        int one=0;
+        int ans=0;
+        if(mp.count(1)){
+            one=mp[1];
+            mp.erase(1);
+            ans=one-((one%2==0)?1:0);
+        }
+        for(auto &it:mp){
+            long long n=it.first;
+            int cnt=1;
+            while(n*n<INT_MAX && mp.count(n*n) && mp[n]>1){
+                cnt++;
+                n*=n;
+                if(mp[n]>1){
+                    continue;
+                }else break;
+            }
+            ans=max(ans,2*cnt-1);
+        }
+        return ans;
+    }
+};
