ea0681f5780cbb0a9ad63320751bcb17ade86d1b
Md Irfan Alam <alamirfan0781@gmail.com>
2026-06-28 12:18:03 +0530

    Time: 68 ms (54.41%), Space: 186.2 MB (67.76%) - LeetHub

--- 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
+++ 3974-maximum-total-sum-of-k-selected-elements/3974-maximum-total-sum-of-k-selected-elements.cpp
@@ -3,13 +3,11 @@ public:
     long long maxSum(vector<int>& nums, int k, int mul) {
         sort(nums.begin(),nums.end(),greater<int>());
         long long ans=0;
-        for(int &x:nums){
-            if(k==0) break;
-            else if(mul>0){
-                ans+=1ll*x*mul;
+        for(int i=0;i<k;i++){
+            if(mul>0){
+                ans+=1LL * nums[i] *mul;
                 mul--;
-            }else ans+=x;
-            k--;
+            }else ans+= nums[i] ;
         }
         return ans;
     }
