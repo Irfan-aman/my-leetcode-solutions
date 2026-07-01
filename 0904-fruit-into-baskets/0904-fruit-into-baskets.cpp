d420ac5b5ce8deac76628415088876c44a14f576
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-01 12:08:35 +0530

    Time: 54 ms (17.7%), Space: 84.8 MB (83.39%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -6,14 +6,19 @@ public:
         int ans = 0;
         while (j < n) {
             mp[fruits[j]]++;
-            while (i < n && mp.size() > 2) {
-                mp[fruits[i]]--;
-                if (mp[fruits[i]] == 0)
-                    mp.erase(fruits[i]);
-                i++;
+            if (mp.size() > 2) {
+                while (i < n && mp.size() > 2) {
+                    mp[fruits[i]]--;
+                    if (mp[fruits[i]] == 0)
+                        mp.erase(fruits[i]);
+                    i++;
+                }
             }
-            ans=max(ans,j-i+1);
-            j++;
+            if (mp.size() <= 2) {
+                ans = max(ans, j - i + 1);
+                j++;
+
+            } 
         }
         return ans;
     }
