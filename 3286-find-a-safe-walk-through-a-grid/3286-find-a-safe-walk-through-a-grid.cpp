7e60ba4b2bceca011435ba0dfb1455e34547ecf0
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-03 00:27:47 +0530

    Time: 19 ms (60.56%), Space: 30.6 MB (79.2%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -2,35 +2,36 @@ class Solution {
 public:
     int dx[4] = {-1, 1, 0, 0};
     int dy[4] = {0, 0, 1, -1};
+    typedef pair<int,pair<int,int>> ppi;
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         if (grid[0][0] && health == 1)
             return false;
         int m = grid.size(), n = grid[0].size();
-        vector<vector<int>> best(m, vector<int>(n, -1));
-        queue<pair<pair<int, int>, int>> q;
-        int startHealth = health - grid[0][0];
-        if (startHealth <= 0) return false;
-        best[0][0] = startHealth;
-        q.push({{0, 0}, startHealth});
-        while (!q.empty()) {
-            int r = q.front().first.first;
-            int c = q.front().first.second;
-            int hlth = q.front().second;
-            if (r == m - 1 && c == n - 1 && hlth > 0)
-                return true;
-            q.pop();
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
+        priority_queue<ppi,vector<ppi>,greater<ppi>> pq;
+        dist[0][0] = grid[0][0];
+        pq.push({dist[0][0], {0,0}});
+        while (!pq.empty()) {
+            int d = pq.top().first;
+            int r = pq.top().second.first;
+            int c = pq.top().second.second;
+            pq.pop();
+            if (r == m - 1 && c == n - 1)
+                return health > d;
+            if(d > dist[r][c])
+                continue;
             for (int i = 0; i < 4; i++) {
                 int x = r + dx[i];
                 int y = c + dy[i];
                 if (x >= 0 && x < m && y >= 0 && y < n) {
-                    int newHealth = hlth - grid[x][y];
-                    if (newHealth <= 0)continue;
-                    if (newHealth <= best[x][y]) continue;
-                    best[x][y] = newHealth;
-                    q.push({{x, y}, newHealth});
+                    if(d+grid[x][y]<dist[x][y]){
+                        dist[x][y]=d+grid[x][y];
+                        pq.push({dist[x][y],{x, y}});
+                    }
+                    
                 }
             }
         }
-        return false;
+        return health>dist[m-1][n-1];
     }
 };
