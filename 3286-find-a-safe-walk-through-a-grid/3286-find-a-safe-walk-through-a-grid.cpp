6eedfe8f06d85406bd23597c8e423e5af01acf05
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-03 00:42:16 +0530

    Time: 8 ms (89.21%), Space: 32.5 MB (48.07%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -2,31 +2,28 @@ class Solution {
 public:
     int dx[4] = {-1, 1, 0, 0};
     int dy[4] = {0, 0, 1, -1};
-    typedef pair<int,pair<int,int>> ppi;
-    bool findSafeWalk(vector<vector<int>>& grid, int health) {
+    bool findSafeWalk(vector<vector<int>>& grid, int health) {//0 1 BFS
         if (grid[0][0] && health == 1)
             return false;
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
-        priority_queue<ppi,vector<ppi>,greater<ppi>> pq;
+        deque<pair<int,int>> dq;
         dist[0][0] = grid[0][0];
-        pq.push({dist[0][0], {0,0}});
-        while (!pq.empty()) {
-            int d = pq.top().first;
-            int r = pq.top().second.first;
-            int c = pq.top().second.second;
-            pq.pop();
+        dq.push_front({0,0});
+        while (!dq.empty()) {
+            auto [r,c]=dq.front();
+            dq.pop_front();
             if (r == m - 1 && c == n - 1)
-                return health > d;
-            if(d > dist[r][c])
-                continue;
+                return health > dist[r][c];
             for (int i = 0; i < 4; i++) {
                 int x = r + dx[i];
                 int y = c + dy[i];
                 if (x >= 0 && x < m && y >= 0 && y < n) {
-                    if(d+grid[x][y]<dist[x][y]){
-                        dist[x][y]=d+grid[x][y];
-                        pq.push({dist[x][y],{x, y}});
+                    if(dist[r][c]+grid[x][y]<dist[x][y]){
+                        dist[x][y]=dist[r][c]+grid[x][y];
+                        if(grid[x][y]){
+                            dq.push_back({x,y});
+                        }else dq.push_front({x, y});
                     }
                     
                 }
