58a31c31cc2fc2dd98bfc4a696a54cb9d84911bc
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-03 19:48:57 +0530

    Time: 0 ms (100%), Space: 10.6 MB (89.07%) - LeetHub

--- 0667-beautiful-arrangement-ii/0667-beautiful-arrangement-ii.cpp
+++ 0667-beautiful-arrangement-ii/0667-beautiful-arrangement-ii.cpp
@@ -2,11 +2,24 @@ class Solution {
 public:
     vector<int> constructArray(int n, int k) {
         vector<int> ans(n,0);
-        for(int i=0;i<n;i++){
-            ans[i]=i+1;
+        int low=1 , high=n;
+        int idx=0;
+        ans[idx++]=low++;
+        bool isHigh=false;
+        while(k>1){
+            ans[idx++]=high--;
+            k--;
+            isHigh=true;
+            if(k>1){
+                ans[idx++]=low++;
+                isHigh=false;
+                k--;
+            }
         }
-        for(int i=1;i<k;i++){
-            reverse(ans.begin()+i,ans.end());
+        while(idx<n){
+            if(isHigh){
+                ans[idx++]=high--;
+            }else ans[idx++]=low++;
         }
         return ans;
     }
