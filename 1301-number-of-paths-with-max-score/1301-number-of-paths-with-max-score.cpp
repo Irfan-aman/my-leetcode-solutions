08508ad003d033533fbeaff0d14d9c3b430bce5b
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-05 21:32:31 +0530

    Time: 15 ms (34.49%), Space: 12.5 MB (45.99%) - LeetHub

--- 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
+++ 1301-number-of-paths-with-max-score/1301-number-of-paths-with-max-score.cpp
@@ -10,54 +10,54 @@ public:
             return {0, 0};
         else if (board[i][j] == 'E')
             return {0, 1};
-        if (dp[i][j] != make_pair(-1,-1))
+        if (dp[i][j] != make_pair(-1, -1))
             return dp[i][j];
-        int leftSum = 0, leftPaths = 0;
-        int upSum = 0, upPaths = 0;
-        int diagSum = 0, diagPaths = 0;
-        if (isValid(i - 1, j)) {//UP
+        int upScore = 0, upPaths = 0;
+        int leftScore = 0, leftPaths = 0;
+        int diagScore = 0, diagPaths = 0;
+
+        if (isValid(i - 1, j)) { // UP
             auto [score, paths] = solve(i - 1, j, board);
-            leftSum = score;
-            leftPaths = paths;
-            if (leftPaths > 0) {
-                leftSum += getIntFromChar(board[i][j]);
-            }
-        }
-        if (isValid(i, j - 1)) {//LEFT
-            auto [score, paths] = solve(i, j - 1, board);
-            upSum = score;
+            upScore = score;
             upPaths = paths;
-            if (upPaths > 0) {
-                upSum += getIntFromChar(board[i][j]);
-            }
-        }
-        if (isValid(i - 1, j - 1)) {//DIAGONAL
-            auto [score, paths] = solve(i - 1, j - 1, board);
-            diagSum = score;
-            diagPaths = paths;
-            if (diagPaths > 0) {
-                diagSum += getIntFromChar(board[i][j]);
-            }
+            if (upPaths > 0)
+                upScore += getIntFromChar(board[i][j]);
         }
 
-        int bestScore = max({leftSum, upSum, diagSum});
+        if (isValid(i, j - 1)) { // LEFT
+            auto [score, paths] = solve(i, j - 1, board);
+            leftScore = score;
+            leftPaths = paths;
+            if (leftPaths > 0)
+                leftScore += getIntFromChar(board[i][j]);
+        }
+
+        if (isValid(i - 1, j - 1)) { // DIAGONAL
+            auto [score, paths] = solve(i - 1, j - 1, board);
+            diagScore = score;
+            diagPaths = paths;
+            if (diagPaths > 0)
+                diagScore += getIntFromChar(board[i][j]);
+        }
+
+        int bestScore = max({upScore, leftScore, diagScore});
         int bestPaths = 0;
 
-        if (leftPaths > 0 && leftSum == bestScore)
-            bestPaths = (bestPaths + leftPaths) % MOD;
-
-        if (upPaths > 0 && upSum == bestScore)
+        if (upPaths > 0 && upScore == bestScore)
             bestPaths = (bestPaths + upPaths) % MOD;
 
-        if (diagPaths > 0 && diagSum == bestScore)
+        if (leftPaths > 0 && leftScore == bestScore)
+            bestPaths = (bestPaths + leftPaths) % MOD;
+
+        if (diagPaths > 0 && diagScore == bestScore)
             bestPaths = (bestPaths + diagPaths) % MOD;
 
-        return dp[i][j]={bestScore, bestPaths};
+        return dp[i][j] = {bestScore, bestPaths};
     }
     vector<int> pathsWithMaxScore(vector<string>& board) {
         int n = board.size();
         N = n;
-        dp.assign(n, vector<pair<int,int>>(n, {-1, -1}));
+        dp.assign(n, vector<pair<int, int>>(n, {-1, -1}));
         auto [maxScore, Paths] = solve(n - 1, n - 1, board);
         return {maxScore, Paths};
     }
