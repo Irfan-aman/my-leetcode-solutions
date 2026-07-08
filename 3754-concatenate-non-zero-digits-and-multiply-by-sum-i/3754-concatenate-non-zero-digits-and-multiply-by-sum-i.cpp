8fee8a3a205f39a60bcd32b9cbecfe706d33e37f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-08 11:00:56 +0530

    Time: 0 ms (100%), Space: 8.9 MB (89.19%) - LeetHub

--- 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
+++ 3754-concatenate-non-zero-digits-and-multiply-by-sum-i/3754-concatenate-non-zero-digits-and-multiply-by-sum-i.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     typedef long long ll;
-    ll reverseInt(int x){
-        ll rev=0;
-        while(x){
-            rev =rev*10 + x%10;
-            x/=10;
-        }
-        return rev;
-    }
     long long sumAndMultiply(int n) {
-        ll x=0 , sum=0;
+        ll x=0 , sum=0 ,mul10=1;
         while(n){
             int dig=n%10;
             if(dig) {
-                x=x*10 + dig;
+                x=mul10*dig +x;
                 sum +=dig;
+                mul10 *=10;
             }
             n /=10;
         }
-        x=reverseInt(x);
         return x*sum;
     }
 };
