0d230ca3067bb6fd836d5d2ecc895de4f82815d1
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-14 23:41:41 +0530

    Time: 1127 ms (46.95%), Space: 292.2 MB (35.65%) - LeetHub

--- 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3336-find-the-number-of-subsequences-with-equal-gcd/3336-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -4,21 +4,25 @@ public:
     int subsequencePairCount(vector<int>& nums) {
         int n = nums.size();
         int maxi=*max_element(nums.begin(),nums.end());
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(maxi+1,vector<int>(maxi+1,0)));
-        for(int i=1;i<=maxi;i++){
-            for(int j=1;j<=maxi;j++){
-                if(i==j)dp[n][i][j]=1;
-                else dp[n][i][j]=0;
-                
+        vector<vector<int>> prev(maxi+1,vector<int>(maxi+1,0));
+        for(int first=1;first<=maxi;first++){
+            for(int second=1;second<=maxi;second++){
+                if(first==second)prev[first][second]=1;
+                else prev[first][second]=0;
             }
         }
         for(int i=n-1;i>=0;i--){
-            for(int j=maxi;j>=0;j--){
-                for(int k=maxi;k>=0;k--){
-                    dp[i][j][k] =(0LL+dp[i+1][j][k]+dp[i+1][__gcd(j,nums[i])][k] +dp[i+1][j][__gcd(k,nums[i])])%MOD;
+            vector<vector<int>> curr(maxi+1,vector<int>(maxi+1,0));
+            for(int first=maxi;first>=0;first--){
+                for(int second=maxi;second>=0;second--){
+                    int skip=prev[first][second];
+                    int take1=prev[__gcd(first,nums[i])][second];
+                    int take2=prev[first][__gcd(second,nums[i])];
+                    curr[first][second] =(0LL+skip+take1+take2)%MOD;
                 }
             }
+            prev=move(curr);
         }
-        return dp[0][0][0];
+        return prev[0][0];
     }
 };
