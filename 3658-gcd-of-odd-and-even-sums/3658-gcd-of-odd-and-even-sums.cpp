14eab83cf079007333cf443c81232da955c46104
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-15 12:28:01 +0530

    Time: 0 ms (100%), Space: 8.4 MB (99.4%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
-        int sumOdd= n*n;// sum of 1st n odd no. = (2*1+ (n-1)2)n/2;
-        int sumEven= (n+1)*n; //sum of 1st n even no. = (2*2+(n-1)2)*n/2;
-        return __gcd(sumOdd,sumEven);
+        // int sumOdd= n*n;// sum of 1st n odd no. = (2*1+ (n-1)2)n/2;
+        // int sumEven= (n+1)*n; //sum of 1st n even no. = (2*2+(n-1)2)*n/2;
+        // return __gcd(sumOdd,sumEven);//gcd(n*n , n*(n+1))=n;
+        return n;
     }
 };
