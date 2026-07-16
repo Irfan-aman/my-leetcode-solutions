61d4f61e64c3a71fca8a09344ab571ca53afff68
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-16 20:38:42 +0530

    Time: 0 ms (100%), Space: 77.1 MB (45.77%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -11,41 +11,27 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        int sum=l1->val + l2->val;
-        int carry=sum/10; sum =sum % 10;
-        ListNode* head=new ListNode(sum);
-        ListNode* temp=head;
-        l1=l1->next; l2=l2->next;
-        while(l1 && l2){
-            int currSum=l1->val + l2->val +carry;
-            carry=currSum/10;
-            currSum = currSum% 10;
-            ListNode* newNode=new ListNode(currSum);
-            temp->next=newNode;
-            temp=newNode;
-            l1=l1->next; l2=l2->next;
-        }
-        while(l1){
-            int currSum=l1->val + carry;
-            carry=currSum/10;
-            currSum = currSum% 10;
-            ListNode* newNode=new ListNode(currSum);
-            temp->next=newNode;
-            temp=newNode;
-            l1=l1->next;
-        }
-        while(l2){
-            int currSum= l2->val +carry;
-            carry=currSum/10;
-            currSum = currSum% 10;
-            ListNode* newNode=new ListNode(currSum);
-            temp->next=newNode;
-            temp=newNode;
-            l2=l2->next;
-        }
-        if(carry){
-            ListNode* newNode=new ListNode(carry);
-            temp->next=newNode;
+        int sum = l1->val + l2->val;
+        int carry = sum / 10;
+        sum = sum % 10;
+        ListNode* head = new ListNode(sum);
+        ListNode* temp = head;
+        l1 = l1->next;
+        l2 = l2->next;
+        while (l1 || l2 || carry) {
+            sum = carry;
+            if (l1) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            carry = sum / 10;
+            sum = sum % 10;
+            temp->next = new ListNode(sum);
+            temp = temp->next;
         }
         return head;
     }
