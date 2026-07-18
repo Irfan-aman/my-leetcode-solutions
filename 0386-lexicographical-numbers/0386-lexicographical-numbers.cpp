dca0fc611b3e4414ab3280c6ee5dcca7ddb3c60e
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-18 14:08:09 +0530

    Time: 0 ms (100%), Space: 14.1 MB (65.95%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    void solve(int num,int n,vector<int> & ans){
+        if(num>n) return;
+        ans.push_back(num);
+        for(int append=0;append<=9;append++){
+            int newNum=num*10+append;
+            if(newNum>n) return;
+            solve(newNum,n,ans);
+        }
+    }
+    vector<int> lexicalOrder(int n) {
+        vector<int> ans;
+        for(int i=1;i<=9 && i<=n;i++){
+            solve(i,n,ans);
+        }
+        return ans;
+    }
+};
