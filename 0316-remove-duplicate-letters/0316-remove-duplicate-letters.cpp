c17a6c135c8728534c191b1bf1643ef9eb12fd8f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-19 12:26:47 +0530

    Time: 0 ms (100%), Space: 9.2 MB (84.28%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<bool> seen(26,false);
+        vector<int> lastIdx(26,-1);
+        for(int i=0;i<s.size();i++){
+            lastIdx[s[i]-'a']=i;
+        }
+        string ans="";
+        for(int i=0;i<s.size();i++){
+            if(seen[s[i]-'a']) continue;
+            while(!ans.empty() && ans.back()>s[i] && lastIdx[ans.back()-'a']>i) {
+                seen[ans.back()-'a']=false;
+                ans.pop_back();
+            }
+            ans.push_back(s[i]);
+            seen[s[i]-'a']=true;
+        }
+        return ans;
+    }
+};
