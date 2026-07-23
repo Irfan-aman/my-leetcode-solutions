a2278b31a66127f3952093a5c5db4e5bd02c0e9f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-23 18:02:32 +0530

    Time: 0 ms (100%), Space: 198.8 MB (85%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -4,9 +4,10 @@ public:
         int n=nums.size();
         if(n==1 ||n==2) return n;
         if((n&(n-1))==0) return 2*n;
-        for(int i=n+1;i<2*n;i++){
-            if((i&(i-1))==0) return i;
+        int ans=1;
+        while(ans<=n){
+            ans<<=1;//ans=ans*2; 1,2,4,8,16,.....
         }
-        return -1;//never returned
+        return ans;
     }
 };
