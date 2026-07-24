7b93351ca3249e4bed9b499b010fb0a8eea272e9
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-25 00:15:57 +0530

    Time: 74 ms (73.34%), Space: 54.8 MB (86.3%) - LeetHub

--- 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
     int maxActiveSectionsAfterTrade(string s) {
-        int n=s.size();
-        vector<int> inactive;//continuous inactive section
-        int active=count(s.begin(),s.end(),'1');
-
-        for(int i=0;i<n;i++){
-            if(s[i]=='0'){
-                int start=i;
-                while(i<n && s[i]=='0')i++;
-                inactive.push_back(i-start);
+        int n = s.size();
+        int left = -1; // for a particular active section, the total number of
+                       // inactive sec to its left
+        int ans = 0;
+        int active = count(s.begin(), s.end(), '1');
+        if(active==0 || n<=2)return active;
+        for (int i = 0; i < n; ) {
+            if (s[i] == '1'){
+                i++;continue;
             }
+            int start = i;
+            while (i < n && s[i] == '0')
+                i++;
+            int right = i - start;
+            if (left != -1) {
+                ans = max(ans, left + right);
+            }
+            left = right;
         }
-        int cnt=0; //store max pair  
-        for(int i=1;i<inactive.size();i++){
-            cnt=max(cnt,inactive[i]+inactive[i-1]);
-        }
-        return cnt+active;
+        return ans + active;
     }
 };
