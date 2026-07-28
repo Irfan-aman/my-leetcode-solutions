a02199d3bf2dc8a5cacc310af6fc8c982401ebea
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-28 18:37:39 +0530

    Time: 106 ms (20.67%), Space: 122.2 MB (28.23%) - LeetHub

--- 2615-sum-of-distances/2615-sum-of-distances.cpp
+++ 2615-sum-of-distances/2615-sum-of-distances.cpp
@@ -8,22 +8,18 @@ public:
             0); // ans[i]= for left(freq[nums[i]]*i - summation
                 // of(i))+ for right(summation of(i)- freq[nums[i]]*i)
 
-        unordered_map<int, ll> mp;   // <nums[i],summation of(i)>
+        unordered_map<int, ll> indexSum;   // <nums[i],summation of(i)>
         unordered_map<int, ll> freq; //<nums[i],freq of nums[i]
         for (int i = 0; i < n; i++) { // left
-            if (mp.count(nums[i])) {
-                ans[i] += freq[nums[i]] * i - mp[nums[i]];
-            }
-            mp[nums[i]] += i;
+            ans[i] += freq[nums[i]] * i - indexSum[nums[i]];
+            indexSum[nums[i]] += i;
             freq[nums[i]]++;
         }
-        mp.clear();
+        indexSum.clear();
         freq.clear();
         for (int i = n - 1; i >= 0; i--) { // right
-            if (mp.count(nums[i])) {
-                ans[i] += mp[nums[i]] - freq[nums[i]] * i;
-            }
-            mp[nums[i]] += i;
+            ans[i] += indexSum[nums[i]] - freq[nums[i]] * i;
+            indexSum[nums[i]] += i;
             freq[nums[i]]++;
         }
 
