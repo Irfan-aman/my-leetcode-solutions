b4287576641d367a0da245a0e0b9620b27e49416
Md Irfan Alam <alamirfan0781@gmail.com>
2026-07-30 12:24:56 +0530

    Time: 0 ms (100%), Space: 8.3 MB (54.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -4,17 +4,23 @@ public:
         int n = word.size();
         if (n <= 8)
             return n;
-        int ans = 0, cnt = 1;
-        while (n) {
-            if (n > 8) {
-                ans += cnt * 8;
-                n -= 8;
-            } else {
-                ans += cnt * n;
-                n = 0;
-            }
-            cnt++;
-        }
-        return ans;
+        else if (n <= 16)
+            return 2 * n - 8; // 8+(n-8)*2
+        else if (n <= 24)
+            return 3 * n - 24; // 8+16+(n-16)*3
+        return 4 * n - 48;     // 8+16+24 +(n-24)*4
     }
 };
+// if (n <= 8)
+//             return n;
+//         int ans = 0, cnt = 1;
+//         while (n) {
+//             if (n > 8) {
+//                 ans += cnt * 8;
+//                 n -= 8;
+//             } else {
+//                 ans += cnt * n;
+//                 n = 0;
+//             }
+//             cnt++;
+//         }
