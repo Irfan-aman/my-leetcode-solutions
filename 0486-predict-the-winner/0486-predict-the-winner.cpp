7a1125ce999821fedfb7f2a1a83b08a8c8c7fa51
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-01 12:40:05 +0530

    Time: 0 ms (100%), Space: 9.5 MB (85.12%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
     int dp[21][21];
-    int solve(int i, int j, vector<int>& nums) {
-        if (i > j)
-            return 0;
-        if (i == j)
-            return nums[i];
-        if (dp[i][j] != -1)
-            return dp[i][j];
-        int take_i = nums[i] - solve(i + 1, j, nums);
-        int take_j = nums[j] - solve(i, j - 1, nums);
-        return dp[i][j] = max(take_i, take_j);
-    }
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(0, n - 1, nums) >= 0;
+        memset(dp, 0, sizeof(dp));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = nums[i];
+        }
+        for (int i = n-1; i >= 0; i--) {
+            for (int j = 1; j < n; j++) {
+                if(j>=i){
+                    int take_i = nums[i] - dp[i + 1][j];
+                int take_j = nums[j] - dp[i][j - 1];
+                dp[i][j] = max(take_i, take_j);
+                }
+            }
+        }
+        return dp[0][n - 1] >= 0;
     }
 };
