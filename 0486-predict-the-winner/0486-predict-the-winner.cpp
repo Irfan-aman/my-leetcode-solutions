763e0c93ad7c54f0366a3c7ed2dd41ecd8ca4e24
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-01 12:43:04 +0530

    Time: 0 ms (100%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -7,13 +7,11 @@ public:
         for (int i = 0; i < n; i++) {
             dp[i][i] = nums[i];
         }
-        for (int i = n-1; i >= 0; i--) {
-            for (int j = 1; j < n; j++) {
-                if(j>=i){
-                    int take_i = nums[i] - dp[i + 1][j];
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                int take_i = nums[i] - dp[i + 1][j];
                 int take_j = nums[j] - dp[i][j - 1];
                 dp[i][j] = max(take_i, take_j);
-                }
             }
         }
         return dp[0][n - 1] >= 0;
