bb335d86d04d324d37a2e16c020c08231e71fc10
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-02 12:20:26 +0530

    Time: 7 ms (35.23%), Space: 11.6 MB (36.14%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -7,8 +7,9 @@ public:
         for (int i = 0; i < n; i++)
             dp[i][i] = piles[i];
         for (int i = n - 2; i >= 0; i--) {
-            for (int j = 1; j < n; j++) {
-                dp[i][j] = max(dp[i + 1][j], dp[i][j - 1]);
+            for (int j = i + 1; j < n; j++) { // i<j
+                dp[i][j] =
+                    max(piles[i] - dp[i + 1][j], piles[j] - dp[i][j - 1]);
             }
         }
         return dp[0][n - 1] > 0;
