c04c24a72caf52cf8d377234159d4e24fbfe61ae
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-03 19:11:28 +0530

    Time: 10 ms (91.69%), Space: 136.1 MB (76.04%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,26 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-    int n;
-    int solve(int i, vector<int>& stoneValue) {
-        if (i >= n)
-            return 0;
-        if (i == n - 1)
-            return stoneValue[n - 1];
-        if (dp[i] != -1)
-            return dp[i];
-        int ans = INT_MIN;
-        int sum = 0;
-        for (int j = 0; j < 3 && i + j < n; j++) {
-            sum += stoneValue[i + j];
-            ans = max(ans, sum - solve(i + j + 1, stoneValue));
-        }
-        return dp[i] = ans;
-    }
     string stoneGameIII(vector<int>& stoneValue) {
-        n = stoneValue.size();
-        dp.resize(n + 1, -1);
-        int diff = solve(0, stoneValue);
+        int n = stoneValue.size();
+        vector<int> dp(n + 1, 0);
+        dp[n - 1] = stoneValue[n - 1];
+        for (int i = n - 1; i >= 0; i--) {
+            int ans = INT_MIN;
+            int sum = 0;
+            for (int j = 0; j < 3 && i + j < n; j++) {
+                sum += stoneValue[i + j];
+                ans = max(ans, sum - dp[i + j + 1]);
+            }
+            dp[i] = ans;
+        }
+        int diff = dp[0];
         if (diff > 0)
             return "Alice";
         else if (diff < 0)
