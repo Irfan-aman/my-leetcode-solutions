b53cd8ba5817ff720107b52972dd5449e40bc57b
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-03 19:22:05 +0530

    Time: 4 ms (98.74%), Space: 130.3 MB (97.03%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -2,18 +2,22 @@ class Solution {
 public:
     string stoneGameIII(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        vector<int> dp(n + 1, 0);
-        dp[n - 1] = stoneValue[n - 1];
+        int a = 0; // dp[i+1]
+        int b = 0; // dp[i+2]
+        int c = 0; // dp[i+3]
         for (int i = n - 1; i >= 0; i--) {
             int ans = INT_MIN;
-            int sum = 0;
-            for (int j = 0; j < 3 && i + j < n; j++) {
-                sum += stoneValue[i + j];
-                ans = max(ans, sum - dp[i + j + 1]);
-            }
-            dp[i] = ans;
+            ans = max(ans, stoneValue[i] - a);
+            if (i + 2 <= n)
+                ans = max(ans, stoneValue[i] + stoneValue[i + 1] - b);
+            if (i + 3 <= n)
+                ans = max(ans, stoneValue[i] + stoneValue[i + 1] +
+                                   stoneValue[i + 2] - c);
+            c = b;
+            b = a;
+            a = ans;
         }
-        int diff = dp[0];
+        int diff = a;
         if (diff > 0)
             return "Alice";
         else if (diff < 0)
