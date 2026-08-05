387ed7ffbb50b3ddd34067c34dc15cbd6396aaaa
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-05 17:51:35 +0530

    Time: 166 ms (79.31%), Space: 301.2 MB (80%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    vector<int> remainingMethods(int n, int k,
+                                 vector<vector<int>>& invocations) {
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n, 0);
+        vector<bool> sus(n, false); // suspecious
+        sus[k] = true;
+        for (auto& it : invocations) {
+            int u = it[0], v = it[1];
+            indegree[v]++;
+            adj[u].push_back(v);
+        }
+        queue<int> q;
+        q.push(k);
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            for (int& nbr : adj[u]) {
+                indegree[nbr]--;
+                if (!sus[nbr]) {
+                    sus[nbr] = true;
+                    q.push(nbr);
+                }
+            }
+        }
+        vector<int> ans;
+        bool flag = false;
+        for (int i = 0; i < n; i++) {
+            if (sus[i] && indegree[i]) {
+                flag = true;
+                break;
+            } else if (!sus[i]) {
+                ans.push_back(i);
+            }
+        }
+        if (flag) {
+            ans.clear();
+            for (int i = 0; i < n; i++) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
