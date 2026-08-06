74b96f48e7bb49f240f18dbbd76e939b27ca4470
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-06 13:47:18 +0530

    Time: 0 ms (100%), Space: 8.7 MB (37.77%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int prodOfDig(int n) {
-        int pod = 1;
+        int prod = 1;
         while (n) {
             if (n % 10 == 0)
                 return 0;
-            pod *= n % 10;
+            prod *= n % 10;
             n /= 10;
         }
-        return pod;
+        return prod;
     }
     int smallestNumber(int n, int t) {
         int ans = n;
-        while (ans <= 100) {
-            int pod = prodOfDig(ans);
+        while (true) {
+            int pod = prodOfDig(ans); // product of digits
             if (pod == 0 || pod % t == 0)
                 return ans;
             ans++;
