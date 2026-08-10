54879bcbb2017a5637e8695b083fffac1a284d97
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-10 21:10:51 +0530

    Time: 27 ms (86.45%), Space: 8.9 MB (82.55%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,20 +1,16 @@
 class Solution {
 public:
-    int dp[100001];
-    bool solve(int n) {
-        if (n == 0)
-            return false;
-        if (dp[n] != -1)
-            return dp[n];
-        for (int i = 1; i * i <= n; i++) {
-            if (dp[n] = solve(n - i * i) == false) {
-                return true;
+    bool winnerSquareGame(int n) {
+        vector<bool> dp(n + 1, false);
+        dp[0] = false; // BAse Case
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                if (dp[i - j * j] == false) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return dp[n] = false;
-    }
-    bool winnerSquareGame(int n) {
-        memset(dp, -1, sizeof(dp));
-        return solve(n);
+        return dp[n];
     }
 };
