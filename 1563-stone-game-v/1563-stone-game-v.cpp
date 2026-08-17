161eb865a4b7e346be9a7a1300f95e20d5d4deb6
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-17 21:16:33 +0530

    Time: 1035 ms (9.19%), Space: 27.9 MB (21.68%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,35 +1,30 @@
 class Solution {
 public:
     vector<int> pref;
-    int dp[501][501];
-    int solve(int l, int r, vector<int>& stoneValue) {
-        if (l >= r) {
-            return 0;
-        }
-        if (dp[l][r] != -1)
-            return dp[l][r];
-        int ans = 0;
-        for (int mid = l; mid < r; mid++) {
-            int left = pref[mid] - ((l >= 1) ? pref[l - 1] : 0);
-            int right = pref[r] - pref[mid];
-            if (left < right) {
-                ans = max(ans, left + solve(l, mid, stoneValue));
-            } else if (left > right) {
-                ans = max(ans, right + solve(mid + 1, r, stoneValue));
-            } else {
-                ans = max(ans, max(left + solve(l, mid, stoneValue),
-                                   right + solve(mid + 1, r, stoneValue)));
-            }
-        }
-        return dp[l][r] = ans;
-    }
     int stoneGameV(vector<int>& stoneValue) {
         int n = stoneValue.size();
-        memset(dp, -1, sizeof(dp));
         pref.push_back(stoneValue[0]);
         for (int i = 1; i < n; i++) {
             pref.push_back(pref.back() + stoneValue[i]);
         }
-        return solve(0, n - 1, stoneValue);
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int l = n - 1; l >= 0; l--) {
+            for (int r = l + 1; r < n; r++) {
+                for (int mid = l; mid < r; mid++) {
+                    int left = pref[mid] - ((l >= 1) ? pref[l - 1] : 0);
+                    int right = pref[r] - pref[mid];
+                    if (left < right) {
+                        dp[l][r] = max(dp[l][r], left + dp[l][mid]);
+                    } else if (left > right) {
+                        dp[l][r] = max(dp[l][r], right + dp[mid+1][r]);
+                    } else {
+                        dp[l][r] = max(dp[l][r],
+                                  max(left + dp[l][mid],
+                                      right + dp[mid+1][r]));
+                    }
+                }
+            }
+        }
+        return dp[0][n-1];
     }
 };
