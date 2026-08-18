4299d6ecebd8f411fd27f84b0097aa03dbbebdb1
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-18 11:05:45 +0530

    Time: 0 ms (100%), Space: 28.7 MB (92.86%) - LeetHub

---
+++ 3471-find-the-largest-almost-missing-integer/3471-find-the-largest-almost-missing-integer.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int largetUniqueInt(vector<int>& nums) {
+        vector<int> freq(51, 0); // idx=nums and freq[i]=freq of nums
+        for (int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+        }
+        for (int i = 50; i >= 1; i--) {
+            if (freq[i] ==1) {
+                return i;
+            }
+        }
+        if (freq[0] == 1)
+            return 0;
+        return -1;
+    }
+    int largestInteger(vector<int>& nums, int k) {
+        int n = nums.size();
+        int ans = -1;
+        if (k == 1)
+            return largetUniqueInt(nums);
+        else if (k == n)
+            return *max_element(nums.begin(), nums.end());
+        int firstNum = nums[0],
+            lastNum = nums[n - 1]; // extreme numbers is the only possible ans
+                                   // for k>1&& k<n
+        int cntFirstN = 0, cntLastN = 0;
+        for (int& x : nums) {
+            if (x == firstNum)
+                cntFirstN++;
+            if (x == lastNum)
+                cntLastN++;
+        }
+        if (lastNum > firstNum) {
+            if (cntLastN == 1)
+                return lastNum;
+            else if (cntFirstN == 1)
+                return firstNum;
+        }else if(firstNum>lastNum){
+            if(cntFirstN==1)return firstNum;
+            else if(cntLastN==1)return lastNum;
+        }
+        return ans;
+    }
+};
