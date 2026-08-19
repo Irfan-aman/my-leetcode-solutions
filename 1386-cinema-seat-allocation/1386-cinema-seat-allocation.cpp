d3922cd68afe082abc09d8a612e145719e350ad9
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-19 21:09:19 +0530

    Time: 18 ms (81.93%), Space: 60.2 MB (73.48%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.cpp
@@ -2,34 +2,26 @@ class Solution {
 public:
     int maxNumberOfFamilies(int n, vector<vector<int>>& reservedSeats) {
         int m = reservedSeats.size();
-        int ans = 0;
-        unordered_map<int, vector<int>> mp;
+        unordered_map<int, int> mp; //<row,bookedSeatMask>
         for (auto& x : reservedSeats) {
-            mp[x[0]].push_back(x[1]);
+            int row = x[0];
+            int seat = x[1];
+            mp[row] |= (1 << seat); // mp[row] | (1<<seat)
         }
-        for (auto& it : mp) {
-            vector<bool> Occupied(11, false);
-            for (int& x : it.second)
-                Occupied[x] = true;
-            bool left = true, middle = true, right = true;
-            for (int i = 2; i <= 5; i++) {
-                if (Occupied[i])
-                    left = false;
-            }
-            for (int i = 4; i <= 7; i++) {
-                if (Occupied[i])
-                    middle = false;
-            }
-            for (int i = 6; i <= 9; i++) {
-                if (Occupied[i])
-                    right = false;
-            }
-            if (left && right)
+        int ans = (n - mp.size()) * 2;
+        int maskA = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5);
+        int maskB = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7);
+        int maskC = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9);
+        for (auto& [row, bookedSeatMask] : mp) {
+            bool groupA = (bookedSeatMask & maskA) == 0;
+            bool groupB = (bookedSeatMask & maskB) == 0;
+            bool groupC = (bookedSeatMask & maskC) == 0;
+            if (groupA && groupC)
                 ans += 2;
-            else if (left || right || middle)
+            else if (groupA || groupB || groupC)
                 ans++;
         }
-        ans += (n - mp.size()) * 2;
+
         return ans;
     }
 };
