cc902ced5c7dade3e4518bd5d6089394f9d2a0e4
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-22 12:31:24 +0530

    Time: 508 ms (49.25%), Space: 26.5 MB (41.19%) - LeetHub

--- 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
+++ 3770-largest-prime-from-consecutive-prime-sum/3770-largest-prime-from-consecutive-prime-sum.cpp
@@ -1,28 +1,29 @@
 class Solution {
 public:
     int largestPrime(int n) {
-        if(n==1)return 0;
+        if (n < 2)
+            return 0;
         if (n == 2)
             return 2;
         vector<bool> prime(n + 1, true);
-        for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n / i; i++) {
             if (prime[i]) {
-                for (int j = i * 2; j <= n; j += i) {
+                for (int j = i * i; j <= n; j += i) {
                     prime[j] = false;
                 }
             }
         }
-        int sum = 0, ans = 0;
-        ;
+        int sum = 0;
+        int ans = 0;
         for (int i = 2; i < n; i++) {
-            if (prime[i]) {
-                sum += i;
-                if (sum > n) {
-                    break;
-                }
-                if (prime[sum]) {
-                    ans = sum;
-                }
+            if (!prime[i])
+                continue;
+            sum += i;
+            if (sum > n) {
+                break;
+            }
+            if (prime[sum]) {
+                ans = sum;
             }
         }
         return ans;
