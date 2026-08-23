08971f795254db0a36f95baabfe472dfccb3143f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-23 10:50:17 +0530

    Time: 56 ms (1.06%), Space: 40.3 MB (8.13%) - LeetHub

---
+++ 2778-sum-of-squares-of-special-elements/2778-sum-of-squares-of-special-elements.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int sumOfSquares(vector<int>& nums) {
+        int n = nums.size();
+        int res = 0;
+        for (int i = 0; i < n; i++) {
+            cout << nums[i] << " ";
+            if (n % (i + 1) == 0) {
+                res += nums[i] * nums[i];
+            }
+        }
+        return res;
+    }
+};
