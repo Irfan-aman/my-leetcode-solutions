65c2a67dde43fa5603534de1be1ea3d5da18d5ef
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-23 16:56:05 +0530

    Time: 3 ms (66.67%), Space: 13.9 MB (67.83%) - LeetHub

---
+++ 1927-sum-game/1927-sum-game.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool sumGame(string num) {
+        int n = num.size();
+        int leftSum = 0, rightSum = 0;
+        int leftQnCount = 0, rightQnCount = 0;
+        for (int i = 0; i < n; i++) {
+            if (i < n / 2) {
+                if (num[i] == '?') {
+                    leftQnCount++;
+                } else {
+                    leftSum += num[i] - '0';
+                }
+            } else {
+                if (num[i] == '?') {
+                    rightQnCount++;
+                } else {
+                    rightSum += num[i] - '0';
+                }
+            }
+        }
+        if ((leftQnCount + rightQnCount) & 1) { // odd '?' count
+            return true;
+        } else { // Even '?' count
+            if (2 * leftSum + 9 * leftQnCount ==
+                2 * rightSum + 9 * rightQnCount) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+};
