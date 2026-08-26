59b76f35b3062a54db11c9d4480bad6fe970087f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-26 13:47:52 +0530

    Time: 0 ms (100%), Space: 8.7 MB (82.87%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -3,52 +3,28 @@ public:
     string shortestBeautifulSubstring(string s, int k) {
         int n = s.size();
         string res = "";
-        int i = 0, j = 0;
-        int oneCount = count(s.begin(), s.end(), '1');
-        if (oneCount == 0) {
-            return "";
-        } else {
-            if (k == 1) {
-                return "1";
-            }
-            oneCount = 0;
-        }
-        while (j < n) {
+        int i = 0;
+        int oneCount = 0;
+        for (int j = 0; j < n; j++) {
             if (s[j] == '1')
                 oneCount++;
             if (oneCount < k) {
-                j++;
                 continue;
             }
-            if (res.size() > k) {
-                while (i < n && oneCount > k) {
-                    if (s[i] == '1')
-                        oneCount--;
-                    i++;
-                }
+            while (i < n && oneCount > k) {
+                if (s[i] == '1')
+                    oneCount--;
+                i++;
+            }
+            if (oneCount == k) {
                 while (s[i] == '0')
                     i++;
             }
-            if (res.empty()) {
-                while (s[i] == '0') {
-                    i++;
-                }
-                res = s.substr(i, j - i + 1);
-            } else if (!res.empty() && res.size() > j - i + 1) {
-                while (s[i] == '0') {
-                    i++;
-                }
-                res = s.substr(i, j - i + 1);
-            } else if (!res.empty() && res.size() == j - i + 1) {
-                while (s[i] == '0') {
-                    i++;
-                }
-                string str = s.substr(i, j - i + 1);
-                if (str < res) {
-                    res = str;
-                }
+            string curr = s.substr(i, j - i + 1);
+            if (res.empty() || (res.size() > curr.size()) ||
+                (res.size() == curr.size() && curr < res)) {
+                res = curr;
             }
-            j++;
         }
         return res;
     }
