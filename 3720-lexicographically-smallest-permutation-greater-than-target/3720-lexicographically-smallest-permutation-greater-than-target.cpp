0ab3daaa329cbb4c6e7c0dd8343e8ffc82801e0c
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-27 16:38:11 +0530

    Time: 1 ms (77.07%), Space: 9.7 MB (90.45%) - LeetHub

---
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string res = "";
+    bool solve(string& curr, vector<int>& freq, string& target, int i,
+               bool greater) {
+        if (i == target.size()) {
+            if (greater) {
+                res = curr;
+                return true;
+            }
+            return false;
+        }
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (freq[ch - 'a'] == 0)
+                continue;
+            if (!greater && ch < target[i])
+                continue;
+
+            bool newGreater = greater || (ch > target[i]);
+            curr.push_back(ch);                                 //  DO
+            freq[ch - 'a']--;                                   //  Do
+            if (solve(curr, freq, target, i + 1, newGreater)) { // EXPLORE
+                return true;
+            }
+            curr.pop_back(); // UNDO
+            freq[ch - 'a']++;
+        }
+        return false;
+    }
+    string lexGreaterPermutation(string s, string target) {
+        int n = s.size();
+        vector<int> freq(26, 0);
+        for (char& ch : s)
+            freq[ch - 'a']++;
+        string curr;
+        solve(curr, freq, target, 0, false);
+        return res;
+    }
+};
