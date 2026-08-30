616f51f745de14dc468a63be94005d63e2c11d5f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-08-30 12:10:06 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.66%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -17,27 +17,19 @@ public:
             return {};
         queue<TreeNode*> q;
         q.push(root);
-        q.push(NULL);
         vector<int> res;
-        int lastElem = 0;
         while (!q.empty()) {
-            TreeNode* curr = q.front();
-            q.pop();
-            if (curr == NULL) {
-                res.push_back(lastElem);
-                if (q.empty())
-                    break;
-                else {
-                    q.push(NULL);
-                    continue;
-                }
-            } else if (curr != NULL)
-                lastElem = curr->val;
-
-            if (curr->left != NULL)
-                q.push(curr->left);
-            if (curr->right != NULL)
-                q.push(curr->right);
+            int n = q.size();
+            TreeNode* curr = NULL;
+            while (n--) {
+                curr = q.front();
+                q.pop();
+                if (curr->left != NULL)
+                    q.push(curr->left);
+                if (curr->right != NULL)
+                    q.push(curr->right);
+            }
+            res.push_back(curr->val);
         }
         return res;
     }
