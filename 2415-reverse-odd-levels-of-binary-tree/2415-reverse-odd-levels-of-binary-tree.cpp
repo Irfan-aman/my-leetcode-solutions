ba430bd10e338d63974919daae9b2b46991be198
Md Irfan Alam <alamirfan0781@gmail.com>
2026-09-05 16:58:52 +0530

    Time: 5 ms (26.47%), Space: 81.9 MB (50.31%) - LeetHub

---
+++ 2415-reverse-odd-levels-of-binary-tree/2415-reverse-odd-levels-of-binary-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* reverseOddLevels(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        bool flag = false;
+        while (!q.empty()) {
+            int size = q.size();
+            vector<TreeNode*> oddNodes;
+            while (size--) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if (curr->left)
+                    q.push(curr->left);
+                if (curr->right)
+                    q.push(curr->right);
+                if (flag)
+                    oddNodes.push_back(curr);
+            }
+            if (flag) {
+                int l = 0, h = oddNodes.size() - 1;
+                while (l < h) {
+                    int x = oddNodes[l]->val;
+                    oddNodes[l]->val = oddNodes[h]->val;
+                    oddNodes[h]->val = x;
+                    l++;
+                    h--;
+                }
+            }
+            flag = !flag;
+        }
+        return root;
+    }
+};
