7b020ef75663c7534d0f3f76bf54d8002f8a6ab4
Md Irfan Alam <alamirfan0781@gmail.com>
2026-09-15 16:07:41 +0530

    Time: 463 ms (13.87%), Space: 216.6 MB (13.87%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,23 +1,27 @@
 class Solution {
 public:
-    bool isPalindrom(string &str, int i, int j) {
-        while (i <= j) {
-            if (str[i] != str[j]) {
-                return false;
-            }
-            i++;
-            j--;
-        }
-        return true;
-    }
     int maxPalindromes(string s, int k) {
         int n = s.size();
-        if(k==1)return n;
-        vector<vector<int>> dp(n+1, vector<int>(n+1, 0));
+        if (k == 1)
+            return n;
+        vector<vector<bool>> palindrome(n + 1, vector<bool>(n + 1, false));
+        for (int L = 1; L <= n; L++) {
+            for (int i = 0; i + L <= n; i++) {
+                int j = i + L - 1;
+                if (i == j) {
+                    palindrome[i][j] = true;
+                } else if (i + 1 == j) {
+                    palindrome[i][j] = s[i] == s[j];
+                } else {
+                    palindrome[i][j] = s[i] == s[j] && palindrome[i + 1][j - 1];
+                }
+            }
+        }
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (isPalindrom(s, i, j)) {
-                    int take = 1 + (j+k<=n?dp[j + 1][j + k]:0);
+                if (palindrome[i][j]) {
+                    int take = 1 + (j + k <= n ? dp[j + 1][j + k] : 0);
                     int grow = dp[i][j + 1];
                     int slide = dp[i + 1][j + 1];
                     dp[i][j] = max({take, grow, slide});
@@ -28,6 +32,6 @@ public:
                 }
             }
         }
-        return dp[0][k-1];
+        return dp[0][k - 1];
     }
 };
