27a232adcd948696d56ac633bce43bb0a68d6b38
Md Irfan Alam <alamirfan0781@gmail.com>
2026-09-19 11:10:49 +0530

    Time: 0 ms (100%), Space: 7.8 MB (35%) - LeetHub

--- 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
+++ 1401-circle-and-rectangle-overlapping/1401-circle-and-rectangle-overlapping.cpp
@@ -2,56 +2,25 @@ class Solution {
 public:
     bool checkOverlap(int radius, int xCenter, int yCenter, int x1, int y1,
                       int x2, int y2) {
-        // check intersection with left vertical line of rect. with circle
-        float k = yCenter * yCenter - radius * radius +
-                  (x1 - xCenter) * (x1 - xCenter);
-        float yRoot1 = yCenter - sqrt(yCenter * yCenter - k);
-        float yRoot2 = yCenter + sqrt(yCenter * yCenter - k);
-        if ((y1 <= yRoot1 && yRoot1 <= y2) || (y1 <= yRoot2 && yRoot2 <= y2)) {
-            return true;
-        }
+        int xi = 0, yi = 0;
+        if (x1 > xCenter)
+            xi = x1;
+        else if (x2 < xCenter)
+            xi = x2;
+        else
+            xi = xCenter;
 
-        // check intersection with right vertical line of rect. with circle
-        k = yCenter * yCenter - radius * radius +
-            (x2 - xCenter) * (x2 - xCenter);
-        yRoot1 = yCenter - sqrt(yCenter * yCenter - k);
-        yRoot2 = yCenter + sqrt(yCenter * yCenter - k);
-        if ((y1 <= yRoot1 && yRoot1 <= y2) || (y1 <= yRoot2 && yRoot2 <= y2)) {
-            return true;
-        }
+        if (y1 > yCenter)
+            yi = y1;
+        else if (y2 < yCenter)
+            yi = y2;
+        else
+            yi = yCenter;
 
-        // check intersection with top horizontal line of rect. with circle
-        k = xCenter * xCenter - radius * radius +
-            (y1 - yCenter) * (y1 - yCenter);
-        float xRoot1 = xCenter - sqrt(xCenter * xCenter - k);
-        float xRoot2 = xCenter - sqrt(xCenter * xCenter - k);
-        ;
-        if ((x1 <= xRoot1 && xRoot1 <= x2) || (x1 <= xRoot2 && xRoot2 <= x2)) {
+        int d = sqrt((xCenter - xi) * (xCenter - xi) +
+                     (yCenter - yi) * (yCenter - yi));
+        if (d <= radius)
             return true;
-        }
-
-        // check intersection with bottom horizontal line of rect. with circle
-        k = xCenter * xCenter - radius * radius +
-            (y2 - yCenter) * (y2 - yCenter);
-        xRoot1 = xCenter - sqrt(xCenter * xCenter - k);
-        xRoot2 = xCenter + sqrt(xCenter * xCenter - k);
-        if ((x1 <= xRoot1 && xRoot1 <= x2) || (x1 <= xRoot2 && xRoot2 <= x2)) {
-            return true;
-        }
-
-        // isCompletely inside of one another
-        // circle is completely inside rect.
-        if ((x1 <= xCenter && xCenter <= x2) &&
-            (y1 <= yCenter && yCenter <= y2)) {
-            return true;
-        }
-        // rect. is completely inside
-        float pt = (x1 - xCenter) * (x1 - xCenter) +
-                   (y1 - yCenter) * (y1 - yCenter) - radius * radius;
-        if (pt <= 0) {
-            return true;
-        }
-
         return false;
     }
 };
