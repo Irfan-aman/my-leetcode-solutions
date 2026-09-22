35064783c5260d16e0c23978d0211b57a383e5ae
Md Irfan Alam <alamirfan0781@gmail.com>
2026-09-22 12:02:49 +0530

    Time: 162 ms (53.57%), Space: 151 MB (70.54%) - LeetHub

--- 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
+++ 3524-find-x-value-of-array-i/3524-find-x-value-of-array-i.cpp
@@ -3,19 +3,17 @@ public:
     typedef long long ll;
     vector<long long> resultArray(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<ll> res(k, 0);
-        vector<ll> prevCnt(k, 0);
+        vector<ll> res(k, 0), prev(k, 0);
         for (int i = 0; i < n; i++) {
-            vector<ll> currentCnt(k, 0);
-            int currentElemRemainder = nums[i] % k;
-            currentCnt[currentElemRemainder]++;
-            for (int oldRem = 0; oldRem < k; oldRem++) {
-                ll newRem = ((ll)oldRem * nums[i]) % k;
-                currentCnt[newRem] += prevCnt[oldRem];
+            vector<ll> curr(k, 0);
+            curr[nums[i] % k]++;
+            for (int oldRem = 0; oldRem <= k - 1; oldRem++) {
+                int newRem = ((ll)oldRem * nums[i]) % k;
+                curr[newRem] += prev[oldRem];
             }
-            prevCnt = move(currentCnt);
-            for (int x = 0; x < k; x++) {
-                res[x] += prevCnt[x];
+            prev = curr;
+            for (int x = 0; x <= k - 1; x++) {
+                res[x] += prev[x];
             }
         }
         return res;
