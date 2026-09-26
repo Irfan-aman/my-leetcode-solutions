5dda68195e8788e0db5bc7cb34e4135b3c06109f
Md Irfan Alam <alamirfan0781@gmail.com>
2026-09-26 11:21:45 +0530

    Time: 75 ms (72.27%), Space: 122.9 MB (46.73%) - LeetHub

--- 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -6,7 +6,7 @@ public:
         for (auto& it : knowledge) {
             mp[it[0]] = it[1];
         }
-        string res="";
+        string res = "";
         for (int i = 0; i < n; i++) {
             if (s[i] == '(') {
                 int st = i;
@@ -16,13 +16,12 @@ public:
                 int end = i;
                 string key = s.substr(st + 1, end - st - 1);
                 if (mp.count(key)) {
-                    for(int j=0;j<mp[key].size();j++){
-                        res.push_back(mp[key][j]);
-                    }
+                    res += mp[key];
                 } else {
                     res.push_back('?');
                 }
-            }else res.push_back(s[i]);
+            } else
+                res.push_back(s[i]);
         }
         return res;
     }
